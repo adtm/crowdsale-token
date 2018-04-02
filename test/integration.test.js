2abc1d106b18723c57d79fab8941f59186d6d16c
adtm <tomas.eglinskas@gmail.com>
2018-04-02 17:07:56 +0300

    Token transfer integration tests
    -transfer tokens
    -fail transfer if not enough funds

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,7 +1,9 @@
 const Token = artifacts.require('./Token.sol');
 
-describe('Token Integration', function(accounts) {
+contract('Token Integration', accounts => {
 	let contract;
+
+	const owner = accounts[0];
 
 	before(async () => {
 		contract = await Token.deployed();
@@ -15,5 +17,30 @@ describe('Token Integration', function(accounts) {
 	it('should pass if the initial token balance is 1 million', async function() {
 		const totalSupply = await contract.totalSupply.call();
 		assert.strictEqual(totalSupply.toString(), '1000000');
+	});
+
+	it('should [transfer] tokens', async function() {
+		const tokenWei = 1000;
+
+		await contract.transfer(accounts[1], tokenWei);
+
+		const ownerBalanceAfter = await contract.balanceOf.call(owner);
+		const recipientBalanceAfter = await contract.balanceOf.call(accounts[1]);
+
+		assert.strictEqual(ownerBalanceAfter.toString(), '999000');
+		assert.strictEqual(recipientBalanceAfter.toString(), '1000');
+	});
+
+	it('should not [transfer] tokens with insufficient funds', async function() {
+		const tokenWei = 10000;
+
+		try {
+			await contract.transfer(accounts[2], tokenWei, { from: accounts[2] });
+		} catch (err) {
+			assert.strictEqual(
+				err.message,
+				'VM Exception while processing transaction: revert'
+			);
+		}
 	});
 });
